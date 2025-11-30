05abc2b0195d97c90fb6d530d27dff982f50d70f
Fahad Alshehri <fahad@Fahads-MacBook-Air.local>
2025-11-30 14:39:18 +0300

    Fix bugs and errors

--- certificate.php
+++ certificate.php
@@ -1,35 +1,57 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") :
-    // check if fields are passed
-    if ($_POST['name'] == null || $_POST['course'] || $_POST['date']) :
-        echo die("<center><span style='color:red'>All fields are required. <a href='index.php'>Home</a></span></center>");
+
+    // Validate required fields
+    if (empty($_POST['name']) || empty($_POST['course']) || empty($_POST['date'])) :
+        die("<center><span style='color:red'>All fields are required. <a href='index.html'>Home</a></span></center>");
     endif;
-    header("Content-Type: image/jpeg");
-    $font = realpath('fonts/PlayfairDisplay-Regular.ttf');
-    $certification = imagecreatefromjpeg('images/certification_template.jpg');
+
+    // Load font and certificate template
+    $font = __DIR__ . '/fonts/PlayfairDisplay-Regular.ttf';
+    $certification = imagecreatefromjpeg(__DIR__ . '/images/certification_template.jpg');
+
+    if (!$certification || !file_exists($font)) {
+        die("Error loading font or template image.");
+    }
+
     $color = imagecolorallocate($certification, 0, 0, 0);
-    // center text on x position
-    $center_text = function ($text, $position) use ($font, $certification) {
-        $bbox = imagettfbbox($position, 0, $font, $text);
-        return (imagesx($certification) / 2) - (($bbox[2] - $bbox[0]) / 2);
+
+    // function to center the text
+    $center_text = function ($text, $size) use ($font, $certification) {
+        $bbox = imagettfbbox($size, 0, $font, $text);
+        $text_width = $bbox[4] - $bbox[0];
+        return (imagesx($certification) / 2) - ($text_width / 2);
     };
-    // paste name on the certificate
+
+    // Write the texts
     $name = $_POST['name'];
     imagettftext($certification, 55, 0, $center_text($name, 55), 650, $color, $font, $name);
-    // paste course name
+
     $course = $_POST['course'];
     imagettftext($certification, 45, 0, $center_text($course, 45), 810, $color, $font, $course);
-    // paste date on the certificate
+
     $date = $_POST['date'];
     imagettftext($certification, 35, 0, 660, 910, $color, $font, $date);
-    // paste certificate id
+
+    // Generate ID
     $certificate_id = rand(1000, 10000);
     imagettftext($certification, 35, 0, 340, 1090, $color, $font, '#' . $certificate_id);
-    // save image in cetfifications folder
-    $filepath = "certifications/" . $name . "_" . $certificate_id . ".jpg";
-    imagejpeg($certification, $filepath);
-    header("Location: $filepath"); // redirecte to certificate
+
+    // Save file
+    $folder = __DIR__ . "/certifications/";
+    if (!is_dir($folder)) mkdir($folder, 0777, true);
+
+    $filepath = $folder . $name . "_" . $certificate_id . ".jpg";
+
+    if (!imagejpeg($certification, $filepath)) {
+        die("Failed to save the certificate.");
+    }
+
     imagedestroy($certification);
+
+    // Redirect to the generated image
+    header("Location: certifications/" . $name . "_" . $certificate_id . ".jpg");
+    exit;
 
 endif;
