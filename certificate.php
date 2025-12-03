e9ff01c8727ad1915db96c04680e5f988872124c
akira61 <otaku.fahad11@gmail.com>
2025-12-03 12:25:12 +0300

    send form via JavaScript

--- certificate.php
+++ certificate.php
@@ -1,10 +1,15 @@
 <?php
+header('Content-Type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") :
 
     // Validate required fields
     if (empty($_POST['name']) || empty($_POST['course']) || empty($_POST['date'])) :
-        die("<center><span style='color:red'>All fields are required. <a href='index.html'>Home</a></span></center>");
+        echo json_encode([
+            "status" => "error",
+            "message" => "All fields are required."
+        ]);
+        exit;
     endif;
 
     // Load font and certificate template
@@ -12,12 +17,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") :
     $certification = imagecreatefromjpeg(__DIR__ . '/images/certification_template.jpg');
 
     if (!$certification || !file_exists($font)) {
-        die("Error loading font or template image.");
+        echo json_encode([
+            "status" => "error",
+            "message" => "Error loading font or template image."
+        ]);
+        exit;
     }
 
     $color = imagecolorallocate($certification, 0, 0, 0);
 
-    // function to center the text
     $center_text = function ($text, $size) use ($font, $certification) {
         $bbox = imagettfbbox($size, 0, $font, $text);
         $text_width = $bbox[4] - $bbox[0];
@@ -34,24 +42,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") :
     $date = $_POST['date'];
     imagettftext($certification, 35, 0, 660, 910, $color, $font, $date);
 
-    // Generate ID
     $certificate_id = rand(1000, 10000);
     imagettftext($certification, 35, 0, 340, 1090, $color, $font, '#' . $certificate_id);
 
-    // Save file
     $folder = __DIR__ . "/certifications/";
     if (!is_dir($folder)) mkdir($folder, 0777, true);
 
     $filepath = $folder . $name . "_" . $certificate_id . ".jpg";
 
     if (!imagejpeg($certification, $filepath)) {
-        die("Failed to save the certificate.");
+        echo json_encode([
+            "status" => "error",
+            "message" => "Failed to save the certificate."
+        ]);
+        exit;
     }
 
     imagedestroy($certification);
 
-    // Redirect to the generated image
-    header("Location: certifications/" . $name . "_" . $certificate_id . ".jpg");
+    // Return the file path
+    echo json_encode([
+        "status" => "success",
+        "file" => "certifications/" . $name . "_" . $certificate_id . ".jpg"
+    ]);
     exit;
 
 endif;
